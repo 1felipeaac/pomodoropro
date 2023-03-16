106c5b5489c064f84810eb6f5ecd47c1ee937494
felipe coelho <felipeaacoelho@gmail.com>
2023-03-16 16:18:46 -0300

    add selectors.js

---
+++ js/selectors.js
@@ -0,0 +1,36 @@
+export const Selectors = {
+
+    btnPlus : document.querySelector("#button-plus"),
+    btnMinun : document.querySelector("#button-minun"),
+    btnPlay : document.querySelector("#button-play"),
+    btnStop : document.querySelector("#button-stop"),
+
+    volumeForest : document.querySelector("#volumeForest"),
+    volumeRain : document.querySelector("#volumeRain"),
+    volumeCoffeeShop : document.querySelector("#volumeCoffeeShop"),
+    volumeFirePlace : document.querySelector("#volumeFirePlace"),
+
+    btnSelector : document.querySelectorAll(".btn-select"),
+    bgAudioVolume : document.querySelectorAll("input[type='range'"),
+
+    minutesDisplay : document.querySelector("#minutes"),
+    secondsDisplay : document.querySelector("#seconds"),
+
+    play : document.querySelector("#button-play"),
+    stop : document.querySelector("#button-stop"),
+    addMinutes : document.querySelector("#button-plus"),
+    removeMinutes : document.querySelector("#button-minun"),
+
+    buttonsSounds : document.querySelector("#sounds"),
+
+}
+
+
+
+
+
+
+
+
+
+
