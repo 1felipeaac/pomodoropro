16c1283c0e0306c672a56667e1f260f7204bc8cd
felipe coelho <felipeaacoelho@gmail.com>
2023-03-16 16:19:40 -0300

    refactor ambientSounds.js

--- js/ambientSounds.js
+++ js/ambientSounds.js
@@ -1,40 +1,66 @@
 import Sound from './sounds.js'
+import {Selectors} from './selectors.js'
 
 const bgAudio = Sound()
 
-const btnSelector = document.querySelectorAll(".btn-select");
-
 function ambientSound(){
-    for(const select of btnSelector){
+    for(const select of Selectors.btnSelector){
         select.addEventListener("change", bgSound)
+    }
+    for(const setVolume of Selectors.bgAudioVolume){
+        setVolume.addEventListener("change", setBgSoundVolume)
     }
 }
 
 function bgSound(e){
-
+    
     if(this.checked){
         switch (this.id) {
             case 'forest':
-                bgAudio.playSound(bgAudio.forest)    
+                setBgSound(bgAudio.forest, Selectors.volumeForest)
                 break;
             case 'rain':
-                bgAudio.playSound(bgAudio.rain)
+                setBgSound(bgAudio.rain, Selectors.volumeRain)
                 break;
             case 'coffeeShop':
-                bgAudio.playSound(bgAudio.coffeeShop)
+                setBgSound(bgAudio.coffeeShop, Selectors.volumeCoffeeShop)
                 break;
             case 'firePlace':
-                bgAudio.playSound(bgAudio.firePlace)
+                setBgSound(bgAudio.firePlace, Selectors.volumeFirePlace)
                 break;
             default:
                 break;
         }
     }
+
+    function setBgSound(theme, themeVolume){
+        bgAudio.playSound(theme)
+        themeVolume.value = "0.5"
+    }
     
 }
 
+function setBgSoundVolume(){
+    switch(this.id){
+        case 'volumeRain':
+            bgAudio.rain.volume = Selectors.volumeRain.value
+            break
+        case 'volumeFirePlace':
+            bgAudio.firePlace.volume = Selectors.volumeFirePlace.value
+            break
+        case 'volumeCoffeeShop':
+            bgAudio.coffeeShop.volume = Selectors.volumeCoffeeShop.value
+            break
+        case 'volumeForest':
+            bgAudio.forest.volume = Selectors.volumeForest.value
+            break
+        default:
+            break;
+    }
+}
+
 function checkedFalse(){
-    for (let select of btnSelector){
+    for (let select of Selectors.btnSelector){
         if(select.checked)
             select.checked = false
     }
