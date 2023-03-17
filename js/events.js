75f9a4c204fec8d10649044836fe85072aed1235
felipe coelho <felipeaacoelho@gmail.com>
2023-03-16 22:56:11 -0300

    import Mode.js

--- js/events.js
+++ js/events.js
@@ -1,8 +1,12 @@
 import { inputTimer, buttonsFunctionalitys } from "./timer.js"
+import Mode from "./modes.js"
+
+const theme = Mode()
 
 export const Events = {
     click(){
         inputTimer()
         buttonsFunctionalitys()
+        theme.eventClick()
     }
 }
